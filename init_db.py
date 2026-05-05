0deaeb82aac06cf9a71535b034eeb758706400e5
AndrewT-21 <andrei.toma18@icloud.com>
2026-05-05 03:19:20 +0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,21 +1,27 @@
-import sqlite3
+import psycopg2
 
-conn = sqlite3.connect('baza_de_date.db')
+conn = psycopg2.connect(
+    host="dpg-d7siafgk1i2s739prs6g-a.oregon-postgres.render.com",
+    database="formular",
+    user="formular_user",
+    password="EboB58d2pBGMVhqXfProSvUDXHfBj08A",
+    port=5432
+)
 cur = conn.cursor()
 
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS inscriere (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nume_familie TEXT NOT NULL,
-        prenume TEXT NOT NULL,
-        varsta INTEGER NOT NULL,
-        telefon TEXT NOT NULL,
-        judet TEXT NOT NULL,
-        oras TEXT NOT NULL,
-        email TEXT NOT NULL,
-        acord_parinti INTEGER
-    )
-''')
+cur.execute("""
+CREATE TABLE IF NOT EXISTS inscriere (
+    id SERIAL PRIMARY KEY,
+    nume_familie TEXT NOT NULL,
+    prenume TEXT NOT NULL,
+    varsta INTEGER NOT NULL,
+    telefon TEXT NOT NULL,
+    judet TEXT NOT NULL,
+    oras TEXT NOT NULL,
+    email TEXT NOT NULL,
+    acord_parinti INTEGER
+)
+""")
 
 conn.commit()
 conn.close()
